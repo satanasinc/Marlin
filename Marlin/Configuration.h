ea41a532e5de4adb958dc44b7e0bb6e16d8093c6
satanasinc <37420357+satanasinc@users.noreply.github.com>
2020-06-16 13:03:22 +0200

    test cambios

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Anet A8 192.168.1.120"
+#define CUSTOM_MACHINE_NAME "Anet A8"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
